9eff2636b532731f41b59be4037f2da45673dd2b
aliabbasishask <aliabbasi7898@gmail.com>
2018-10-25 15:43:20 +0330

    last hassans

--- src/Defense.java
+++ src/Defense.java
@@ -2,17 +2,28 @@ public class Defense extends Building {
 
 	private int power;
 
-	public void getPower() {
-		// TODO - implement Defense.getPower
-		throw new UnsupportedOperationException();
+	public Defense(){
+		this.power = 30;
+		this.cost = 10000;
 	}
 
-	/**
-	 * 
-	 * @param power
-	 */
 	public void setPower(int power) {
 		this.power = power;
+	}
+
+	public int getPower() {
+		return power;
+	}
+
+	@Override
+	public void upgrade() {
+		if (this.level <= 4 && this.level > 1) {
+			this.power += 0;
+			this.cost += 5000;
+		}
+		else {
+			System.out.println("not possible");
+		}
 	}
 
 }
